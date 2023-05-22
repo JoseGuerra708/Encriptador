69651f896e8978db3a08d25eec775d9362f2e496
Jose M Guerra <chicomen708@hotmail.com>
2023-05-22 11:03:21 -0500

    Challenge #1

---
+++ script.js
@@ -0,0 +1,52 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replace(new RegExp(matrizCodigo[i][0], "g"), matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value)
+    mensaje.value = textoDesencriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replace(new RegExp(matrizCodigo[i][1], "g"), matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+function copiarMensaje( ) {
+    const mensajeTextArea = document.querySelector(".mensaje");
+    mensajeTextArea.select();
+    navigator.clipboard.writeText(mensajeTextArea.value)
+        .then(() => console.log('Texto copiado'))
+        .catch((err) => console.error('Error al copiar el texto: ', err));
+}
